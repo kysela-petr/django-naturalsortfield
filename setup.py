d670530e2689c9b344122e1cea2998d2e9e00b52
Emil Santurio <emilsas@users.noreply.github.com>
2019-02-08 18:10:03 -0300

    Update django-naturalsortfield setup

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ import naturalsortfield
 
 setup(
     name='django-naturalsortfield',
-    url='https://github.com/nathforge/django-naturalsortfield',
+    url='https://github.com/emilsas/django-naturalsortfield',
     version=naturalsortfield.version_string(),
     description='Better ordering for Django CharFields.',
     long_description=open('README.txt').read(),
     
-    author='Nathan Reynolds',
-    author_email='email@nreynolds.co.uk',
+    author='Emil Santurio',
+    author_email='emilsas@gmail.com',
     
     packages=['naturalsortfield'],
     package_dir={'': PACKAGE_PATH},
