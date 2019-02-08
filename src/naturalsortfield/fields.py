c66a455f862a356e925fce9dbb0f71ca8b1f56b3
Emil Santurio <emilsas@users.noreply.github.com>
2019-02-08 17:59:47 -0300

    Add deconstruct method to append for_field arg

--- src/naturalsortfield/fields.py
+++ src/naturalsortfield/fields.py
@@ -1,5 +1,6 @@
-from django.db import models
 import re
+
+from django.db import models
 
 
 class NaturalSortField(models.CharField):
@@ -8,19 +9,29 @@ class NaturalSortField(models.CharField):
         kwargs.setdefault('db_index', True)
         kwargs.setdefault('editable', False)
         kwargs.setdefault('max_length', 255)
+        kwargs.setdefault('default', None)
+        kwargs.setdefault('null', True)
         super(NaturalSortField, self).__init__(**kwargs)
-    
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(NaturalSortField, self).deconstruct()
+        args.append(self.for_field)
+        return name, path, args, kwargs
+
     def pre_save(self, model_instance, add):
-        return self.naturalize(getattr(model_instance, self.for_field))
-    
-    def naturalize(self, string):
+        field_value = getattr(model_instance, self.for_field)
+        field_value = str(field_value)
+
+        return self.naturalize(field_value)
+
+    @staticmethod
+    def naturalize(string):
         def naturalize_int_match(match):
             return '%08d' % (int(match.group(0)),)
-        
+
         string = string.lower()
         string = string.strip()
         string = re.sub(r'^the\s+', '', string)
         string = re.sub(r'\d+', naturalize_int_match, string)
-        
-        return string
 
+        return string
